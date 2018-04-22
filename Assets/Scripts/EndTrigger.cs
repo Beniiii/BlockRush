0ddda47d51498d027ebbbe205064f1cbf915c27e
Benjamin Huber <benjamin.huber96@gmx.ch>
2018-04-22 16:54:29 +0200

    Initialized

---
+++ Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour {
+
+    public GameManager gameManager;
+
+    private void OnTriggerEnter()
+    {
+        Debug.Log("LEVEL ENDED");
+        gameManager.CompleteLevel();
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+        FindObjectOfType<AudioManager>().Play("LevelComplete");
+    }
+
+    public void EndGame()
+    {
+        if(gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
